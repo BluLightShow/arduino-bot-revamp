a5238c2bb7d43210c18a1b434856b56ed4f733a5
MaderDash <32471649+MaderDash@users.noreply.github.com>
2022-10-17 15:34:30 -0500

    updating from the web

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -79,4 +79,129 @@ export default {
         ),
     ],
   },
+  ESPcomm: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle("Solving your ESP board's communication errors (Try these in order)")
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: "1. Is your Serial monitor open?",
+          value:"If it is, close it. This allows your IDE to upload sketches without conflicts with the Serial Monitor.",
+        },
+        {
+          name: "2. Have you selected the right port in your IDE? ",
+          value: "You could have selected something that is **not** your ESP. Change the port in the Arduino IDE by going into Tools -> Port.",
+        },
+        {
+          name: "3. Have you selected the right board in your IDE?",
+          value: "You need to select the right board and model.",
+        },
+        {
+        name: "4. Is your Serial monitor showing gibberish?",
+        value: "Try setting it to the correct baud rate (At the bottom right corner of the Serial monitor). Most examples use 115200 baud.",
+        },
+        {
+        name: "5. Is anything connected to your Tx and Rx pins?",
+        value: "If there is, try removing everything connected to them.",
+        },
+        {
+          name: "6. Have you tried holding down the BOOT/FLASH button?",
+            value:  "Try Holding down the BOOT/FLASH button before uploading.",
+          
+        },
+        {
+          name:  "7. Are there any problems with your wiring?",
+            value:"Check if there are any wiring errors or loose connections (Note:- if you are using an FTDI Board, FTDI TX and RX pins must be cross-connected with the ESP Tx and Rx pins(TX goes to RX)).",
+          
+        },
+        {
+          name:  "8. Cant see your ESP's COM Port?",
+          value:  "This often means you don’t have the USB drivers installed. Take a closer look at the USB chip and check its name, Go to Google and search for your particular chip to find the drivers and install them in your operating system. If you have the CP2102 chip, here are the [official drivers](https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers).",
+          
+        },
+        {
+          name:  "Still not fixed?",
+          value:  "Check out these troubleshooting guides for your respective boards: [ESP32](https://randomnerdtutorials.com/esp32-troubleshooting-guide/#:~:text=When%20you%20try%20to%20upload,button%20in%20your%20ESP32%20board), [ESP32 CAM](https://randomnerdtutorials.com/esp32-cam-troubleshooting-guide/#:~:text=If%20you%20get%20this%20exact,times%2C%20might%20solve%20the%20issue.) and [ESP8266](https://randomnerdtutorials.com/esp8266-troubleshooting-guide/).",
+          
+        },
+      ),
+    ],
+  },
+  language: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle("What coding language does arduino IDE use? What language should you learn?")
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: "Arduino IDE code is normally C++11",
+          value: "The language most often used in Arduino IDE for programming is primarily C++11. There are some added functions that are very useful for using the Arduino platform. They can be found at https://www.arduino.cc/reference/en/.",
+        },
+      ),
+    ],
+  },
+  liberror: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle("Solving library errors (such as \"yourlib.h not found\")")
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: "1. Is your Library installed?",
+          value:
+            "Go to the Library Manager (Sketch -> Include Library -> Manage Libraries), search for the library, and install it. If it is already installed, try re-installing it through the library manager (Sketch -> include Library -> Manage Libraries).",
+        }),
+    ],
+  },
+  power: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle("Powering an arduino")
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: "1. How much power can an arduino provide?",
+          value: "Most PC USB ports are limited to 500ma. The Arduino has a fuse on the board to help protect your PC, and the Arduino from shorts. This is limited to 400ma. If you try to draw more current than this, the fuse will get hot and stop the short.",
+        },
+        {
+          name:  "2. How much power can each pin of the arduino provide?",
+          value:  "Each pin of the Arduino is rated 20ma. In many cases, if you do not provide a resistor to the circuit on a pin, then you can damage the Arduino. We use resistors for LED's or any other component that can draw more than 20ma. Motors should not be driven directly off of the arduino PIN's.",
+        },
+        {
+          name: "3. Powering the Arduino properly",
+          value: "On most Arduino boards there will be a pin marked VIN. This stands for Voltage Input. You can provide the max power rating for your board on this pin. UNO will accept 7-12v. If you have a regulated 5 volt power supply, you can sometimes use the 5v pin to power the Arduino. You should NOT connect batteries to the 5v or 3.3v pins. You can also power Arduino's by the USB plugs, or the Barrel jack on some boards.",
+        },
+      ),
+    ],
+  },
+  pullup: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle("What does pullup (or pulldown) mean, and how do I use it?")
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: "Pins used as INPUT should not be left unconnected (floating).",
+          value: "This can lead to undesired behavior. If input pins are connected to digital sensors then the sensor itself usually keeps the pin either HIGH or LOW.",
+        },
+        {
+          name: "Pins used with switches (pushbuttons or other) should use a resistor to 'pull' the pin HIGH or LOW.",
+          value: "If the pin is connected to Vcc through a resistor it is said to be 'pulled up', if the pin goes to ground through a resistor it is said to be 'pulled down'. When a resistor is used this way the input is held at either Vcc or ground when the switch is not closed so that it is never 'floating'. Pins that are pulled down are normally LOW and go HIGH when the switch (wired to Vcc) is closed. Pins that are pulled up are normally HIGH and go LOW when the switch (wired to ground) is closed. (The resistor, often 10K, allows only a small current to flow from Vcc to ground when the switch is closed, if the pin was tied directly to a power rail closing the switch would short out the power supply!)",
+        },
+        {
+          name: "Many chips include internal resistors so that an external resistor doesn't need to be added to your circuit.",
+          value: "On the ATmega328P chips used on many Arduino boards you can pull up the pin by using `pinMode(pin, INPUT_PULLUP)`. If the pin is declared this way it is normally HIGH and all that is needed is a switch wired from the pin to ground. When the switch is closed the pin will go LOW. The example below shows pin 2 setup this way.",
+        },
+        //"image": "https://www.arduino.cc/wiki/static/f7e18e95df4a8d274fc9129fa60eb428/928ea/PullUp.png"
+      ),
+    ],
+  },
+  ask: {
+    embeds: [
+      new MessageEmbed(universalEmbed)
+        .setTitle('Dont ask to ask - Just ask!')
+        .addFields({
+          // TODO: Turn slash command reference into a button
+          name: 'Describe what your code/hardware does and what you want it to do instead. Sharing is caring! Share your code, use /tag `name: codeblock` to learn how.',
+          value:
+            'Keep in mind: no one here is paid to help you, so the least you can do is to refine your question in a proper language.',
+        }),
+    ],
+  },
 };
